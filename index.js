b8989fa6c520ccbde232ca0c6d033df24ef946c8
Oluwatobi Yusuf <Oluwatobi.Yusuf@wakanow.local>
2023-03-13 22:50:29 +0100

    milestone1-created alert message

---
+++ index.js
@@ -0,0 +1,36 @@
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+
+// Form Event Listener
+searchForm.addEventListener('submit', e => {
+    // Get search term
+    const searchTerm = searchInput.value;
+    // Get Sort
+    const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+    // Get limit 
+    const searchLimit = document.getElementById("limit").value;
+    console.log(searchLimit)
+
+    if (searchTerm === "") {
+        // show message
+        showMessage('Please add a search Term', 'alert-danger')
+    }
+    e.preventDefault();
+});
+
+// show message
+function showMessage(message, className) {
+    // create div
+    const div = document.createElement('div');
+    // Add Classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent 
+    const searchContainer = document.getElementById('search-container');
+    // Get child
+    const search = document.getElementById('search');
+    // show message
+    searchContainer.insertBefore(div, search);
+
+}
